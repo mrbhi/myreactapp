2ccd6a4e1ffff702d2c35aef370827aab114b808
mrbhi <taofeeqworld@hotmail.com>
2023-10-11 12:09:00 -0700

    Updated README file, logo, title

--- src/component/Header.js
+++ src/component/Header.js
@@ -1,12 +1,12 @@
 import React from "react";
-import logo from "../logo.png"
+import logo from "../btesz.png"
 
 const New = () => {
     return (
         <nav className="nav-bar">
             <img className="nav-icon" alt="nav icon" src={logo} />
-            <h3 className="nav-logo-text">MY PROFILE</h3>
-            <h4 className="nav-title">LEFT HAND BAR</h4>
+            <h3 className="nav-logo-text">BTESZ</h3>
+            <h4 className="nav-title">MENU BAR</h4>
         </nav>
     )
 }
